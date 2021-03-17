41cd499b7dc8312b6e07aefc6061f8fa3f2a0bdb
Pavel Prystinka <pprystinka@microsoft.com>
2021-03-17 15:07:49 -0700

    Add PhoneNumbers sample app

---
+++ PhoneNumbers/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Communication.PhoneNumbers;
+using Azure.Communication.PhoneNumbers.Models;
+
+namespace PhoneNumbers
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Get a connection string to our Azure Communication resource.
+            var connectionString = "<connection_string>";
+            var client = new PhoneNumbersClient(connectionString);
+
+            // Search phone numbers
+            var capabilities = new PhoneNumberCapabilities(calling:PhoneNumberCapabilityType.None, sms:PhoneNumberCapabilityType.Outbound);
+            var searchOptions = new PhoneNumberSearchOptions { AreaCode = "833", Quantity = 1 };
+
+            var searchOperation = await client.StartSearchAvailablePhoneNumbersAsync("US", PhoneNumberType.TollFree, PhoneNumberAssignmentType.Application, capabilities, searchOptions);
+            await searchOperation.WaitForCompletionAsync();
+
+            var phoneNumber = searchOperation.Value.PhoneNumbers.First();
+
+            // Purchase searched phone numbers
+            var purchaseOperation = await client.StartPurchasePhoneNumbersAsync(searchOperation.Value.SearchId);
+            await purchaseOperation.WaitForCompletionAsync();
+
+            // Get purchased phone number(s)
+            var getPhoneNumberResponse = await client.GetPhoneNumberAsync(phoneNumber);
+            Console.WriteLine($"Phone number: {getPhoneNumberResponse.Value.PhoneNumber}, country code: {getPhoneNumberResponse.Value.CountryCode}");
+
+            var purchasedPhoneNumbers = client.GetPhoneNumbersAsync();
+            await foreach (var purchasedPhoneNumber in purchasedPhoneNumbers)
+            {
+                Console.WriteLine($"Phone number: {purchasedPhoneNumber.PhoneNumber}, country code: {purchasedPhoneNumber.CountryCode}");
+            }
+
+            // Update capabilities
+            var updateCapabilitiesOperation = await client.StartUpdateCapabilitiesAsync(phoneNumber, calling: PhoneNumberCapabilityType.Outbound, sms: PhoneNumberCapabilityType.InboundOutbound);
+            await updateCapabilitiesOperation.WaitForCompletionAsync();
+
+            // Release phone number
+            var releaseOperation = await client.StartReleasePhoneNumberAsync(phoneNumber);
+            await releaseOperation.WaitForCompletionAsync();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -4,10 +4,12 @@ Azure Communication Services enable developers to add communication capabilities
 
 ## Prerequisites
 
-- An Azure account with an active subscription. [Create an account for free](https://azure.microsoft.com/free/?WT.mc_id=A261C142F). 
+- An Azure account with an active subscription. [Create an account for free](https://azure.microsoft.com/free/?WT.mc_id=A261C142F).
 - The latest version [.NET Core client library](https://dotnet.microsoft.com/download/dotnet-core) for your operating system.
 - An active Communication Services resource and connection string. [Create a Communication Services resource](https://docs.microsoft.com/en-us/azure/communication-services/quickstarts/create-communication-resource?tabs=windows&pivots=platform-azp).
 
 ## Quickstarts
 
 1. [Send SMS](https://docs.microsoft.com/en-us/azure/communication-services/quickstarts/telephony-sms/send?pivots=programming-language-csharp)
+
+2. [Manage Phone Numbers](https://docs.microsoft.com/en-us/azure/communication-services/quickstarts/telephony-sms/get-phone-number?pivot=programming-language-csharp)
