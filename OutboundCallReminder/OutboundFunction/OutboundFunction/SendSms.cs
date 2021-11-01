26d13777f069ef0ca0bfed1ea5a580a48f563b23
Ravi Thanneeru <v-ratann@microsoft.com>
2021-11-01 09:48:02 -0700

    Azure function for outbound call using calling server SDK and send SMS

---
+++ OutboundCallReminder/OutboundFunction/OutboundFunction/SendSms.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Azure.Communication.Sms;
+using System.Threading.Tasks;
+using Azure;
+
+namespace OutboundFunction
+{
+    class SendSMS
+    {
+        private SmsClient smsClient;
+        public SendSMS()
+        {
+            string connectionString = Environment.GetEnvironmentVariable("Connectionstring");
+            smsClient = new SmsClient(connectionString);
+        }
+
+        public async void SendOneToOneSms(string sourcePhoneNumber, string targetPhoneNumber, string message)
+        {
+            try
+            {
+                Response<SmsSendResult> sendResult = await smsClient.SendAsync(
+                    from: sourcePhoneNumber,
+                    to: targetPhoneNumber,
+                    message: message
+                );
+                Logger.LogMessage(Logger.MessageType.INFORMATION,$"Sms id: {sendResult.Value.MessageId}");
+            }
+            catch (Exception ex)
+            {
+                Logger.LogMessage(Logger.MessageType.ERROR, $"Send message failed unexpectedly, reason: {ex.Message}");
+            }
+        }
+    }
+}
