51b9ab2aaf161aceb09a8cf2bd602c9c2f3d591d
Mohammad Irfan <v-moirf@microsoft.com>
2022-05-24 11:38:47 -0700

    Initial add - email sample

---
+++ SendEmail/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Communication.Email;
+using Azure.Communication.Email.Models;
+
+namespace SendEmail
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // This code demonstrates how to send email using Azure Communication Services.
+            string connectionString = "<ACS_CONNECTION_STRING>";
+            EmailClient emailClient = new EmailClient(connectionString);
+            string subject = "Send email quick start - dotnet";
+            var emailContent = new EmailContent(subject)
+            {
+                PlainText = "This is plain mail send test body \n Best Wishes!!",
+                Html = "<html><body><h1>Quick send email test</h1><br/><h4>Communication email as a service mail send app working properly</h4><p>Happy Learning!!</p></body></html>"
+            };
+            string sender = "<SENDER_EMAIL>";
+
+            EmailRecipients emailRecipients = new EmailRecipients(new List<EmailAddress> {
+                new EmailAddress("<RECIPIENT_EMAIL>") { DisplayName = "<RECIPINENT_DISPLAY_NAME>" }
+            });
+
+            EmailMessage emailMessage = new EmailMessage(sender, emailContent, emailRecipients);
+
+            try
+            {
+                var sendEmailResponse = emailClient.Send(emailMessage);
+
+                string messageId = sendEmailResponse.Value.MessageId;
+                if (!string.IsNullOrEmpty(messageId))
+                {
+                    Console.WriteLine($"Email Sent, MessageId = {messageId}");
+                }
+                else
+                {
+                    Console.WriteLine($"Failed to send Email, response: {sendEmailResponse.GetRawResponse()}");
+                    return;
+                }
+
+                // wait max 2 minutes to check the send status for mail.
+                var cancellationToken = new CancellationTokenSource(TimeSpan.FromMinutes(2));
+                do
+                {
+                    var sendStatus = emailClient.GetSendStatus(messageId);
+                    Console.WriteLine($"Send mail status for MessageId : <{messageId}>, Status: [{sendStatus.Value.Status}]");
+
+                    if (sendStatus.Value.Status != SendStatus.Queued)
+                    {
+                        break;
+                    }
+                    await Task.Delay(TimeSpan.FromSeconds(10));
+                    
+                } while (!cancellationToken.IsCancellationRequested);
+
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    Console.WriteLine($"Looks like we timed out for email");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in sending email, {ex}");
+            }
+        }
+    }
+}
