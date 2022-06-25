8b4cc58540af93f24be739230dae219f8744cd3c
lookinnovative <anthony.robinson@lookinnovative.com>
2022-06-25 10:17:30 -0400

    ChatQuickstart

---
+++ ChatQuickstart/Program.cs
@@ -0,0 +1,20 @@
+﻿using Azure;
+using Azure.Communication;
+using Azure.Communication.Chat;
+using System;
+
+namespace ChatQuickstart
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            // Your unique Azure Communication service endpoint
+            Uri endpoint = new Uri("https://<RESOURCE_NAME>.communication.azure.com");
+
+            CommunicationTokenCredential communicationTokenCredential = new CommunicationTokenCredential(<Access_Token>);
+            ChatClient chatClient = new ChatClient(endpoint, communicationTokenCredential);
+        }
+    }
+}
+
