b7503841709d9cbf1d0ba9d3b2939f9b71310b5e
lookinnovative <anthony.robinson@lookinnovative.com>
2022-06-25 16:18:56 -0400

    update

--- CommunicationAccessTokensQuickstart/Program.cs
+++ CommunicationAccessTokensQuickstart/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Azure.Communication.Identity;
+using Microsoft.Identity.Client;
+
+namespace CommunicationAccessTokensQuickstart
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Azure Communication Services - Teams Access Tokens Quickstart");
+
+            // Quickstart code goes here
+        }
+    }
+}
+
+
