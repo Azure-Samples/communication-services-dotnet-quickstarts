348b728fe099ca5e5950f355f859e8c3aad0fb33
williamzhao87 <williamzhao87@gmail.com>
2022-07-13 11:51:44 -0400

    Add teams user interop

--- IncomingCallRouting/IncomingCallRouting/Utils/Constants.cs
+++ IncomingCallRouting/IncomingCallRouting/Utils/Constants.cs
@@ -10,5 +10,6 @@ namespace IncomingCallRouting
     {
         public const string userIdentityRegex = @"8:acs:[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}_[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}";
         public const string phoneIdentityRegex = @"^\+\d{10,14}$";
+        public const string teamsIdentityRegex = @"orgid:[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}";
     }
 }
