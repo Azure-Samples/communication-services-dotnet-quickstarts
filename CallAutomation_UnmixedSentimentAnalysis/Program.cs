a9b9bfc53d2059ab226aa250f702964f9984df57
williamzhao87 <williamzhao87@gmail.com>
2022-11-30 22:28:23 -0500

    Add Unmixed Audio sentiment analysis sample

---
+++ CallAutomation_UnmixedSentimentAnalysis/Program.cs
@@ -0,0 +1,131 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Nodes;
+using Azure.Communication.CallAutomation;
+using Azure.Messaging;
+using Azure.Messaging.EventGrid;
+using Azure.Messaging.EventGrid.SystemEvents;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var client = new CallAutomationClient("<ACS_CONNECTION_STRING>");
+var callbackUriBase = "<NGROK_URI>";
+var mediaFileSource = new Uri("<WELCOME_WAV_FILE_URI>");
+var app = builder.Build();
+var serverCallId = "";
+
+#region Handle Incoming Call
+
+app.MapPost("/api/incomingCall", async (
+    [FromBody] EventGridEvent[] eventGridEvents) =>
+{
+    foreach (var eventGridEvent in eventGridEvents)
+    {
+        // Handle system events
+        if (eventGridEvent.TryGetSystemEventData(out object eventData))
+        {
+            // Handle the subscription validation event.
+            if (eventData is SubscriptionValidationEventData subscriptionValidationEventData)
+            {
+                var responseData = new SubscriptionValidationResponse
+                {
+                    ValidationResponse = subscriptionValidationEventData.ValidationCode
+                };
+                return Results.Ok(responseData);
+            }
+        }
+
+        // Handle incoming call event
+        var jsonObject = JsonNode.Parse(eventGridEvent.Data).AsObject();
+        var callerId = (string)(jsonObject["from"]["rawId"]);
+        var incomingCallContext = (string)jsonObject["incomingCallContext"];
+        var callbackUri = new Uri(callbackUriBase + $"/api/calls/{Guid.NewGuid()}?callerId={callerId}");
+
+        AnswerCallResult answerCallResult = await client.AnswerCallAsync(incomingCallContext, callbackUri);
+        serverCallId = answerCallResult.CallConnectionProperties.ServerCallId;
+    }
+    return Results.Ok();
+});
+
+#endregion
+
+#region Handle Call Connected
+
+app.MapPost("/api/calls/{contextId}", async (
+    [FromBody] CloudEvent[] cloudEvents,
+    [FromRoute] string contextId,
+    [Required] string callerId) =>
+{
+    foreach (var cloudEvent in cloudEvents)
+    {
+        CallAutomationEventBase @event = CallAutomationEventParser.Parse(cloudEvent);
+        
+        if (@event is CallConnected)
+        {
+            // Option 1: Record unmixed audio and automatically set participant ordering based on audio first detected
+            await client.GetCallRecording().StartRecordingAsync(
+                new StartRecordingOptions(new ServerCallLocator(serverCallId))
+                {
+                    RecordingChannel = RecordingChannel.Unmixed,
+                });
+
+            // Option 2: Record unmixed audio and manually set the PSTN participant to be the first channel in the recording
+            // var participants = await client.GetCallConnection(callConnected.CallConnectionId).GetParticipantsAsync();
+            // var caller = participants.Value.FirstOrDefault(p => p.Identifier is PhoneNumberIdentifier).Identifier;
+            //
+            // await client.GetCallRecording().StartRecordingAsync(
+            //     new StartRecordingOptions(new ServerCallLocator(serverCallId))
+            //     {
+            //         RecordingChannel = RecordingChannel.Unmixed,
+            //         AudioChannelParticipantOrdering = { caller }
+            //     });
+
+            await client.GetCallConnection(@event.CallConnectionId)
+                .GetCallMedia()
+                .PlayToAllAsync(new FileSource(mediaFileSource));
+        }
+
+    }
+    return Results.Ok();
+}).Produces(StatusCodes.Status200OK);
+
+#endregion
+
+#region Download Recording
+
+app.MapPost("/api/recordingDone", async ([FromBody] EventGridEvent[] eventGridEvents) =>
+{
+    foreach (var eventGridEvent in eventGridEvents)
+    {
+        // Handle system events
+        if (eventGridEvent.TryGetSystemEventData(out object eventData))
+        {
+            // Handle the subscription validation event
+            if (eventData is SubscriptionValidationEventData subscriptionValidationEventData)
+            {
+                var responseData = new SubscriptionValidationResponse
+                {
+                    ValidationResponse = subscriptionValidationEventData.ValidationCode
+                };
+                return Results.Ok(responseData);
+            }
+        }
+
+        // Handle recording status updated event - download recording to local folder
+        var jsonObject = JsonNode.Parse(eventGridEvent.Data).AsObject();
+        var downloadUri = (string)jsonObject["recordingStorageInfo"]["recordingChunks"][0]["contentLocation"];
+        await using var fileStream = File.Create("../../../unmixed_recording.wav");
+        await client.GetCallRecording().DownloadToAsync(new Uri(downloadUri), fileStream);
+    }
+    return Results.Ok();
+});
+
+#endregion
+
+#region Sentiment Analysis
+
+app.MapGet("/api/sentimentAnalysis", async ([FromQuery] string filePath) => await SentimentAnalysis.AnalyzeSentiment(filePath));
+
+#endregion
+
+app.Run();
---
+++ CallAutomation_UnmixedSentimentAnalysis/SentimentAnalysis.cs
@@ -0,0 +1,147 @@
+﻿using System.Text.Json.Serialization;
+using Azure;
+using Azure.AI.TextAnalytics;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
+public class SentimentAnalysis
+{
+    private static readonly Uri CognitiveServicesUri = new ("<COGNITIVE_SERVICE_URI>");
+    private static readonly AzureKeyCredential CognitiveServicesKey = new ("<COGNITIVE_SERVICE_KEY>");
+    private static readonly string SpeechKey = "<SPEECH_KEY>";
+    private static readonly string SpeechRegion = "<SPEECH_REGION>";
+
+    public static async Task<Dictionary<string, SentimentResult>> AnalyzeSentiment(string filePath)
+    {
+        var recognizedText = await SpeechToTextFromFile(filePath);
+        var textAnalysisClient = new TextAnalyticsClient(CognitiveServicesUri, CognitiveServicesKey);
+
+        var response = await textAnalysisClient.AnalyzeSentimentAsync(recognizedText);
+        var sentiments = GetSentiments(response.Value);
+
+        return sentiments;
+    }
+
+    /**
+     * <summary>
+     * Speech to text using file input.
+     * </summary>
+     */
+    private static async Task<string> SpeechToTextFromFile(string filePath)
+    {
+        var speechConfig = SpeechConfig.FromSubscription(SpeechKey, SpeechRegion);
+
+        // Creates an intent recognizer using file as audio input.
+        using var audioInput = AudioConfig.FromWavFileInput(filePath);
+        using var recognizer = new SpeechRecognizer(speechConfig, audioInput);
+
+        // The TaskCompletionSource to stop recognition.
+        var stopRecognition = new TaskCompletionSource<int>(TaskCreationOptions.RunContinuationsAsynchronously);
+        var recognizedSpeech = "";
+
+        // Subscribes to events.
+        recognizer.Recognizing += (s, e) =>
+        {
+            Console.WriteLine($"RECOGNIZING: Text={e.Result.Text}");
+        };
+
+        recognizer.Recognized += (s, e) =>
+        {
+            if (e.Result.Reason == ResultReason.RecognizedSpeech)
+            {
+                Console.WriteLine($"RECOGNIZED: Text={e.Result.Text}");
+                recognizedSpeech += $" {e.Result.Text}";
+            }
+            else if (e.Result.Reason == ResultReason.NoMatch)
+            {
+                Console.WriteLine("NOMATCH: Speech could not be recognized.");
+            }
+        };
+
+        recognizer.Canceled += (s, e) =>
+        {
+            Console.WriteLine($"CANCELED: Reason={e.Reason}");
+
+            if (e.Reason == CancellationReason.Error)
+            {
+                Console.WriteLine($"CANCELED: ErrorCode={e.ErrorCode}");
+                Console.WriteLine($"CANCELED: ErrorDetails={e.ErrorDetails}");
+                Console.WriteLine("CANCELED: Did you update the subscription info?");
+            }
+
+            stopRecognition.TrySetResult(0);
+        };
+
+        recognizer.SessionStarted += (s, e) =>
+        {
+            Console.WriteLine("\n    Session started event.");
+        };
+
+        recognizer.SessionStopped += (s, e) =>
+        {
+            Console.WriteLine("\n    Session stopped event.");
+            Console.WriteLine("\nStop recognition.");
+            stopRecognition.TrySetResult(0);
+        };
+
+        // Starts continuous recognition. Uses StopContinuousRecognitionAsync() to stop recognition.
+        await recognizer.StartContinuousRecognitionAsync().ConfigureAwait(false);
+
+        // Waits for completion.
+        Task.WaitAny(stopRecognition.Task);
+
+        // Stops recognition.
+        await recognizer.StopContinuousRecognitionAsync().ConfigureAwait(false);
+
+        return recognizedSpeech;
+    }
+
+    /**
+     * <summary>
+     * Get sentiment from text.
+     * </summary>
+     */
+    private static Dictionary<string, SentimentResult> GetSentiments(DocumentSentiment documentSentiment)
+    {
+        var sentiments = new Dictionary<string, SentimentResult>();
+
+        Console.WriteLine($"Results of \"Sentiment Analysis\" Model\n");
+        Console.WriteLine($"Document sentiment is {documentSentiment.Sentiment}, with confidence scores: ");
+        Console.WriteLine($"  Positive confidence score: {documentSentiment.ConfidenceScores.Positive}.");
+        Console.WriteLine($"  Neutral confidence score: {documentSentiment.ConfidenceScores.Neutral}.");
+        Console.WriteLine($"  Negative confidence score: {documentSentiment.ConfidenceScores.Negative}.");
+        Console.WriteLine("");
+        Console.WriteLine($"  Sentence sentiment results:");
+
+        sentiments.Add("Overall Sentiment", new SentimentResult
+        {
+            Sentiment = documentSentiment.Sentiment,
+            ConfidenceScores = documentSentiment.ConfidenceScores
+        });
+
+        foreach (SentenceSentiment sentimentInSentence in documentSentiment.Sentences)
+        {
+            Console.WriteLine($"  For sentence: \"{sentimentInSentence.Text}\"");
+            Console.WriteLine($"  Sentiment is {sentimentInSentence.Sentiment}, with confidence scores: ");
+            Console.WriteLine($"    Positive confidence score: {sentimentInSentence.ConfidenceScores.Positive}.");
+            Console.WriteLine($"    Neutral confidence score: {sentimentInSentence.ConfidenceScores.Neutral}.");
+            Console.WriteLine($"    Negative confidence score: {sentimentInSentence.ConfidenceScores.Negative}.");
+            Console.WriteLine("");
+            sentiments.Add(sentimentInSentence.Text, new SentimentResult
+            {
+                Sentiment = sentimentInSentence.Sentiment,
+                ConfidenceScores = sentimentInSentence.ConfidenceScores
+            });
+        }
+
+        return sentiments;
+    }
+
+    public class SentimentResult
+    {
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public TextSentiment Sentiment { get; set; }
+    
+        public SentimentConfidenceScores ConfidenceScores { get; set; }
+    }
+}
