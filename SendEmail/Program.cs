f53a780975dc76dc06b7298e4e28cc22dfd38a6b
apattath <avin.pattath.318@gmail.com>
2023-03-02 14:38:33 -0800

    Update SendEmail sample

--- SendEmail/Program.cs
+++ SendEmail/Program.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
+﻿using Azure.Communication.Email;
+using System;
 using System.Threading.Tasks;
-using Azure.Communication.Email;
-using Azure.Communication.Email.Models;
 
 namespace SendEmail
 {
@@ -16,52 +13,27 @@ namespace SendEmail
             var emailClient = new EmailClient(connectionString);
 
             var subject = "Send email quick start - dotnet";
-            var emailContent = new EmailContent(subject)
-            {
-                PlainText = "This is plain mail send test body \n Best Wishes!!",
-                Html = "<html><body><h1>Quick send email test</h1><br/><h4>Communication email as a service mail send app working properly</h4><p>Happy Learning!!</p></body></html>"
-            };
+            var htmlContent = "<html><body><h1>Quick send email test</h1><br/><h4>Communication email as a service mail send app working properly</h4><p>Happy Learning!!</p></body></html>";
             var sender = "<SENDER_EMAIL>";
-
-            var emailRecipients = new EmailRecipients(new List<EmailAddress> {
-                new EmailAddress("<RECIPIENT_EMAIL>") { DisplayName = "<RECIPINENT_DISPLAY_NAME>" }
-            });
-
-            var emailMessage = new EmailMessage(sender, emailContent, emailRecipients);
+            var recipient = "<RECIPIENT_EMAIL>";
 
             try
             {
-                SendEmailResult sendEmailResult = emailClient.Send(emailMessage);
+                Console.WriteLine("Sending email...");
+                EmailSendOperation emailSendOperation = await emailClient.SendAsync(Azure.WaitUntil.Completed, sender, recipient, subject, htmlContent);
+                EmailSendResult statusMonitor = emailSendOperation.Value;
 
-                string messageId = sendEmailResult.MessageId;
-                if (!string.IsNullOrEmpty(messageId))
+                string operationId = emailSendOperation.Id;
+                var emailSendStatus = statusMonitor.Status;
+
+                if (emailSendStatus == EmailSendStatus.Succeeded)
                 {
-                    Console.WriteLine($"Email sent, MessageId = {messageId}");
+                    Console.WriteLine($"Email sent. \n OperationId = {operationId}. \n Status = {emailSendStatus}");
                 }
                 else
                 {
-                    Console.WriteLine($"Failed to send email.");
+                    Console.WriteLine($"Failed to send email. \n OperationId = {operationId}. \n Status = {emailSendStatus}");
                     return;
-                }
-
-                // wait max 2 minutes to check the send status for mail.
-                var cancellationToken = new CancellationTokenSource(TimeSpan.FromMinutes(2));
-                do
-                {
-                    SendStatusResult sendStatus = emailClient.GetSendStatus(messageId);
-                    Console.WriteLine($"Send mail status for MessageId : <{messageId}>, Status: [{sendStatus.Status}]");
-
-                    if (sendStatus.Status != SendStatus.Queued)
-                    {
-                        break;
-                    }
-                    await Task.Delay(TimeSpan.FromSeconds(10));
-                    
-                } while (!cancellationToken.IsCancellationRequested);
-
-                if (cancellationToken.IsCancellationRequested)
-                {
-                    Console.WriteLine($"Looks like we timed out for email");
                 }
             }
             catch (Exception ex)
