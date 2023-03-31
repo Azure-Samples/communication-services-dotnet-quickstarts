a31e3cc4762ebcd455245368051ce18814d894a3
David de Matheu <dademath@microsoft.com>
2023-03-31 14:45:21 -0700

    fixes

--- sms-url-shortener/SendSMSUrlShortener.cs
+++ sms-url-shortener/SendSMSUrlShortener.cs
@@ -22,40 +22,35 @@ namespace Company.Function
 
     public static class SendSMSUrlShortener
     {
-        static string connectionString = Environment.GetEnvironmentVariable("ACS_CONNECTIONSTRING", EnvironmentVariableTarget.Process);
-        static string phoneNumberFrom = Environment.GetEnvironmentVariable("ACS_PHONE_NUMBER", EnvironmentVariableTarget.Process); // Ex. +15555555555
-        static string urlShortener = Environment.GetEnvironmentVariable("URL_SHORTENER", EnvironmentVariableTarget.Process); // Ex. https://<Azure Function URL>/api/UrlCreate
         [FunctionName("SendSMSUrlShortener")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
-
-            SmsClient smsClient = new SmsClient(connectionString);
-
+            
+            //Parse Query Parameters
             string phoneNumberTo = req.Query["phoneNumber"];
             string urlToShorten = req.Query["url"];
 
+            //Get short URL from Azure URL Shortener
             using var client = new HttpClient();
-            
             var requestData = new
             {
                 Url = urlToShorten
             };
-
             var requestBody = JsonSerializer.Serialize(requestData);
-
             var httpContent = new StringContent(requestBody, Encoding.UTF8, "application/json");
-
+            string urlShortener = Environment.GetEnvironmentVariable("URL_SHORTENER", EnvironmentVariableTarget.Process); // Ex. https://<Azure Function URL>/api/UrlCreate
             var response = await client.PostAsync(urlShortener, httpContent);
-
             var content = await response.Content.ReadAsStringAsync();
-
             var data = System.Text.Json.JsonSerializer.Deserialize<ShortenedUrl>(content);
-
             var url = data.ShortUrl;
 
+            //Send SMS with Azure Communication Services
+            string connectionString = Environment.GetEnvironmentVariable("ACS_CONNECTIONSTRING", EnvironmentVariableTarget.Process);
+            string phoneNumberFrom = Environment.GetEnvironmentVariable("ACS_PHONE_NUMBER", EnvironmentVariableTarget.Process); // Ex. +15555555555
+            SmsClient smsClient = new SmsClient(connectionString);
             SmsSendResult sendResult = smsClient.Send(
                 from: phoneNumberFrom,
                 to: phoneNumberTo,
