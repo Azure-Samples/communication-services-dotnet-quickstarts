4cdca9ea83984093d65057db2c6fee01bdddc24b
Fariba Haghbin <fhaghbin@microsoft.com>
2023-05-26 15:02:27 -0400

    Added callautomation outbound calling

---
+++ CallAutomation_OutboundCalling/CallAutomation_OutboundCalling/Program.cs
@@ -0,0 +1,107 @@
+using Azure.Communication;
+using Azure.Communication.CallAutomation;
+using Azure.Messaging;
+using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Your ACS resource connection string
+var acsConnectionString = "<ACS_CONNECTION_STRING>";
+
+// Your ACS resource phone number will act as source number to start outbound call
+var acsPhonenumber = "<ACS_PHONE_NUMBER>";
+
+// Target phone number you want to receive the call.
+var targetPhonenumber = "<TARGET_PHONE_NUMBER>";
+
+// Base url of the app
+var callbackUriHost = Environment.GetEnvironmentVariable("VS_TUNNEL_URL")?.TrimEnd('/');
+
+CallAutomationClient callAutomationClient = new CallAutomationClient(acsConnectionString);
+builder.Services.AddSingleton(callAutomationClient);
+var app = builder.Build();
+
+app.MapPost("/api/outboundCall", async (ILogger<Program> logger) =>
+{
+    PhoneNumberIdentifier target = new PhoneNumberIdentifier(targetPhonenumber);
+    PhoneNumberIdentifier caller = new PhoneNumberIdentifier(acsPhonenumber);
+    CallInvite callInvite = new CallInvite(target, caller);
+    CreateCallResult createCallResult = await callAutomationClient.CreateCallAsync(callInvite, new Uri(callbackUriHost + "/api/callbacks")).ConfigureAwait(false);
+
+    logger.LogInformation($"Created call with connection id: {createCallResult.CallConnectionProperties.CallConnectionId}");
+});
+
+app.MapPost("/api/callbacks", async (CloudEvent[] cloudEvents, ILogger<Program> logger) =>
+{
+    foreach (var cloudEvent in cloudEvents)
+    {
+        CallAutomationEventBase @event = CallAutomationEventParser.Parse(cloudEvent);
+        logger.LogInformation($"{@event?.GetType().Name} event received for call connection id: {@event?.CallConnectionId}");
+        var callConnection = callAutomationClient.GetCallConnection(@event.CallConnectionId);
+        var callMedia = callConnection.GetCallMedia();
+
+        if (@event is CallConnected)
+        {
+            logger.LogInformation($"Start Recording...");
+            CallLocator callLocator = new ServerCallLocator(@event.ServerCallId);
+            _ = await callAutomationClient.GetCallRecording().StartAsync(new StartRecordingOptions(callLocator));
+
+            // prepare recognize tones
+            CallMediaRecognizeDtmfOptions callMediaRecognizeDtmfOptions = new CallMediaRecognizeDtmfOptions(new PhoneNumberIdentifier(targetPhonenumber), maxTonesToCollect: 1);
+            callMediaRecognizeDtmfOptions.Prompt = new FileSource(new Uri(callbackUriHost + "/audio/MainMenu.wav"));
+            callMediaRecognizeDtmfOptions.InterruptPrompt = true;
+            callMediaRecognizeDtmfOptions.InitialSilenceTimeout = TimeSpan.FromSeconds(5);
+            callMediaRecognizeDtmfOptions.InterToneTimeout = TimeSpan.FromSeconds(10);
+            callMediaRecognizeDtmfOptions.StopTones = new List<DtmfTone> { DtmfTone.Pound, DtmfTone.Asterisk };
+
+            // Send request to recognize tones
+            await callMedia.StartRecognizingAsync(callMediaRecognizeDtmfOptions);
+        }
+        if (@event is RecognizeCompleted)
+        {
+            // Play audio once recognition is completed sucessfully
+            var recognizeCompleted = (RecognizeCompleted)@event;
+
+            string selectedTone = ((DtmfResult)recognizeCompleted.RecognizeResult).ConvertToString();
+
+            switch (selectedTone)
+            {
+                case "1":
+                    await callMedia.PlayToAllAsync(new FileSource(new Uri(callbackUriHost + "/audio/Confirmed.wav")));
+                    break;
+
+                case "2":
+                    await callMedia.PlayToAllAsync(new FileSource(new Uri(callbackUriHost + "/audio/Goodbye.wav")));         
+                    break;
+
+                default:
+                    await callConnection.HangUpAsync(true);
+                    break;
+            }
+        }
+        if ((@event is RecognizeFailed) || (@event is PlayCompleted) || (@event is PlayFailed))
+        {
+            logger.LogInformation($"Terminating Call.");
+            await callConnection.HangUpAsync(true);
+        }
+    }
+    return Results.Ok();
+}).Produces(StatusCodes.Status200OK);
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+           Path.Combine(builder.Environment.ContentRootPath, "audio")),
+    RequestPath = "/audio"
+});
+
+app.UseHttpsRedirection();
+app.Run();
---
+++ CallAutomation_OutboundCalling/readme.md
@@ -0,0 +1,49 @@
+﻿|page_type|languages|products
+|---|---|---|
+|sample|<table><tr><td>csharp</tr></td></table>|<table><tr><td>azure</td><td>azure-communication-services</td></tr></table>|
+
+# Call Automation - Quick Start Sample
+
+This sample application shows how the Azure Communication Services  - Call Automation SDK can be used to build IVR related solutions. 
+It makes an outbound call to a phone number, performs DTMF recognition, plays a different audio message based on the key pressed by the callee and hangs-up the call. 
+This sample application configured for accepting tone 1 (tone1), 2 (tone2) , If the callee pressed any other key than expected, the call will be disconnected.
+This sample application is also capable of making multiple concurrent outbound calls. The application is a web-based application built on .Net7 framework.
+
+# Design
+
+![design](./data/OutboundCallDesign.png)
+
+## Prerequisites
+
+- Create an Azure account with an active subscription. For details, see [Create an account for free](https://azure.microsoft.com/free/)
+- [Visual Studio (2022 v17.4.0 and above)](https://visualstudio.microsoft.com/vs/)
+- [.NET7 Framework](https://dotnet.microsoft.com/en-us/download/dotnet/7.0) (Make sure to install version that corresponds with your visual studio instance, 32 vs 64 bit)
+- Create an Azure Communication Services resource. For details, see [Create an Azure Communication Resource](https://docs.microsoft.com/azure/communication-services/quickstarts/create-communication-resource). You will need to record your resource **connection string** for this sample.
+- Get a phone number for your new Azure Communication Services resource. For details, see [Get a phone number](https://learn.microsoft.com/en-us/azure/communication-services/quickstarts/telephony/get-phone-number?tabs=windows&pivots=programming-language-csharp)
+- Enable Visual studio dev tunneling for local development. For details, see [Enable dev tunnel] (https://learn.microsoft.com/en-us/connectors/custom-connectors/port-tunneling)
+	- To enable dev tunneling, Click `Tools` -> `Options` in Visual Studio 2022
+	- In the search bar type tunnel, Click the checkbox under `Environment` -> `Preview Features` called `Enable dev tunnels for Web Application`
+	- ![EnableDevTunnel](./data/EnableDevTunnel.png) 
+	- Login into your account under `Dev Tunnels` -> `General`
+	- ![LogInDevTunnel](./data/AddAccountForTunnel.png) 
+
+## Before running the sample for the first time
+
+1. Open an instance of PowerShell, Windows Terminal, Command Prompt or equivalent and navigate to the directory that you would like to clone the sample to.
+2. git clone `https://github.com/Azure-Samples/Communication-Services-dotnet-quickstarts.git`.
+3. Navigate to `CallAutomation_OutboundCalling` folder and open `CallAutomation_OutboundCalling.sln` file.
+
+### Configuring application
+
+Open the Program.cs file to configure the following settings
+
+1. `acsConnectionString`: Azure Communication Service resource's connection string.
+2. `acsPhonenumber`: Phone number associated with the Azure Communication Service resource. For e.g. "+1425XXXAAAA"
+3. `targetPhonenumber`: Target phone number to add in the call. For e.g. "+1425XXXAAAA"
+4. `callbackUriHost`: Base url of the app. (For local development replace the dev tunnel url)
+
+### Run app locally
+
+1. Run the `CallAutomation_OutboundCalling` project in debug mode
+2. Browser should pop up with swagger UI, If using Visual studio dev tunnels note the app base url and update it in the Program.cs file `(callbackUriHost)`
+3. To initiate the call, from the swagger ui execute the `/api/outboundCall` endpoint or make a Http post request to `https://<callbackUriHost>/api/outboundCall`
