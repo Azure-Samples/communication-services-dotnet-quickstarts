4f651d18b12c9f409ed28769acd4413eb166909a
Yassir Bisteni <91511620+yassirbisteni@users.noreply.github.com>
2023-07-07 01:10:55 -0600

    Added RawVideo Quickstart

---
+++ RawVideo/BufferExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Windows.Foundation;
+using Windows.Storage.Streams;
+
+namespace CallingTestApp
+{
+    [ComImport]
+    [Guid("5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    unsafe interface IMemoryBufferByteAccess
+    {
+        void GetBuffer(out byte* buffer, out uint capacity);
+    }
+
+    [ComImport]
+    [Guid("905A0FEF-BC53-11DF-8C49-001E4FC686DA")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    unsafe interface IBufferByteAccess
+    {
+        void Buffer(out byte* buffer);
+    }
+
+    internal static class BufferExtensions
+    {
+        public static unsafe byte* GetArrayBuffer(IMemoryBuffer memoryBuffer)
+        {
+            IMemoryBufferReference memoryBufferReference = memoryBuffer.CreateReference();
+            var memoryBufferByteAccess = memoryBufferReference as IMemoryBufferByteAccess;
+
+            memoryBufferByteAccess.GetBuffer(out byte* arrayBuffer, out uint arrayBufferCapacity);
+
+            GC.AddMemoryPressure(arrayBufferCapacity);
+
+            return arrayBuffer;
+        }
+
+        public static unsafe byte* GetArrayBuffer(IBuffer buffer)
+        {
+            var bufferByteAccess = buffer as IBufferByteAccess;
+
+            bufferByteAccess.Buffer(out byte* arrayBuffer);
+            uint arrayBufferCapacity = buffer.Capacity;
+
+            GC.AddMemoryPressure(arrayBufferCapacity);
+
+            return arrayBuffer;
+        }
+    }
+}
---
+++ RawVideo/VideoFrameRenderer.cs
@@ -0,0 +1,73 @@
+﻿using Azure.Communication.Calling.WindowsClient;
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace CallingTestApp
+{
+    internal class VideoFrameRenderer : IDisposable
+    {
+        private static readonly string MediaEncodingSubtypes_NV12 = "{3231564E-0000-0010-8000-00AA00389B71}";
+
+        private MediaElement mediaElement;
+        private Image imageElement;
+        private MediaPlayerElement mediaPlayerElement;
+        private StackPanel parentView;
+        private UIElement childView;
+
+        public enum RenderType
+        {
+            MediaElement,
+            ImageElement,
+            MediaPlayerElement
+        }
+
+        public VideoFrameRenderer(StackPanel parentView, int w, int h)
+        {
+            childView = imageElement = new Image
+            {
+                Width = w,
+                Height = h,
+                Visibility = Visibility.Visible
+            };
+
+            this.parentView = parentView;
+            parentView.Children.Add(childView);
+        }
+
+        public unsafe void RenderRawVideoFrame(RawVideoFrameBuffer videoFrameBuffer)
+        {
+            VideoStreamFormat videoStreamFormat = videoFrameBuffer.StreamFormat;
+            int w = videoStreamFormat.Width;
+            int h = videoStreamFormat.Height;
+
+            var writeableBitmap = new WriteableBitmap(videoStreamFormat.Width, videoStreamFormat.Height);
+
+            byte* sourceArrayBuffer = BufferExtensions.GetArrayBuffer(videoFrameBuffer.Buffers[0]);
+            byte* destArrayBuffer = BufferExtensions.GetArrayBuffer(writeableBitmap.PixelBuffer);
+
+            int rgbaCapacity = w * h * 4;
+
+            for (int i = 0; i < rgbaCapacity; i += 4)
+            {
+                destArrayBuffer[i + 0] = sourceArrayBuffer[i + 2];
+                destArrayBuffer[i + 1] = sourceArrayBuffer[i + 1];
+                destArrayBuffer[i + 2] = sourceArrayBuffer[i + 0];
+                destArrayBuffer[i + 3] = sourceArrayBuffer[i + 3];
+            }
+
+            imageElement.Source = writeableBitmap;
+        }
+
+        public void ClearView()
+        {
+            imageElement.Source = null;
+        }
+
+        public void Dispose()
+        {
+            ClearView();
+        }
+    }
+}
