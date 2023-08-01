c51f5ffae971e5f322024ca6448e6913231e0bab
Jacques de Broin <jdebroin@microsoft.com>
2023-08-01 18:21:52 -0400

    3324141 Send Dtmf sample

---
+++ CallAutomation_SendDtmfTones/Program.cs
@@ -0,0 +1,71 @@
+using Azure.Communication;
+using Azure.Communication.CallAutomation;
+using Azure.Messaging;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Your ACS resource connection string
+        var acsConnectionString = "<ACS_CONNECTION_STRING>";
+        
+        // Your ACS resource phone number will act as source number to start outbound call
+        var acsPhonenumber = "<ACS_PHONE_NUMBER>";
+
+        // Target phone number you want to receive the call.
+        var targetPhonenumber = "<TARGET_PHONE_NUMBER>";
+
+        // Base url of the app
+        var callbackUriHost = "<CALLBACK_URI_HOST_WITH_PROTOCOL>";
+
+        var callAutomationClient = new CallAutomationClient(acsConnectionString);
+        var app = builder.Build();
+
+        app.MapGet("/outboundCall", async (ILogger<Program> logger) =>
+        {
+            var target = new PhoneNumberIdentifier(targetPhonenumber);
+            var caller = new PhoneNumberIdentifier(acsPhonenumber);
+            var callInvite = new CallInvite(target, caller);
+            var createCallResult = await callAutomationClient.CreateCallAsync(callInvite, new Uri(callbackUriHost + "/api/callbacks"));
+            logger.LogInformation("CreateCallAsync result: {createCallResult}", createCallResult);
+        });
+
+        app.MapPost("/api/callbacks", async (CloudEvent[] cloudEvents, ILogger<Program> logger) =>
+        {
+            foreach (var cloudEvent in cloudEvents)
+            {
+                CallAutomationEventBase parsedEvent = CallAutomationEventParser.Parse(cloudEvent);
+                logger.LogInformation("Received event {eventName} for call connection id {callConnectionId}", parsedEvent?.GetType().Name, parsedEvent?.CallConnectionId);
+                var callConnection = callAutomationClient.GetCallConnection(parsedEvent?.CallConnectionId);
+                var callMedia = callConnection.GetCallMedia();
+
+                if (parsedEvent is CallConnected)
+                {
+                    // Send DTMF tones
+                    var tones = new DtmfTone[] { DtmfTone.One, DtmfTone.Two, DtmfTone.Three };
+                    var targetParticipant = new PhoneNumberIdentifier(targetPhonenumber);
+                    var operationContext = "Consultant IVR";
+                    var sendDtmfAsyncResult = await callMedia.SendDtmfAsync(tones, targetParticipant, operationContext);
+                    logger.LogInformation("SendDtmfAsync result: {sendDtmfAsyncResult}", sendDtmfAsyncResult);
+                }
+                else if (parsedEvent is SendDtmfCompleted sendDtmfCompleted)
+                {
+                    logger.LogInformation("SendDtmf completed successfully");
+                    await callConnection.HangUpAsync(true);
+                }
+                else if (parsedEvent is SendDtmfFailed sendDtmfFailed)
+                {
+                    logger.LogInformation("SendDtmf failed with ResultInformation: {resultInformation}", sendDtmfFailed.ResultInformation);
+                    await callConnection.HangUpAsync(true);
+                }
+            }
+            return Results.Ok();
+        }).Produces(StatusCodes.Status200OK);
+
+        app.UseStaticFiles();
+
+        app.Run();
+    }
+}
