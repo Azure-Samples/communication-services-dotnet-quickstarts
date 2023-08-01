9a38eb0f81ddd1f056a0aa698698046cab1bd49f
Jacques de Broin <jdebroin@microsoft.com>
2023-08-01 19:03:12 -0400

    3324141 ContinuousDtmfRecognition sample

---
+++ CallAutomation_ContinuousDtmfRecognition/Program.cs
@@ -0,0 +1,70 @@
+using Azure.Communication;
+using Azure.Communication.CallAutomation;
+using Azure.Messaging;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Your ACS resource connection string
+        var acsConnectionString = "<ACS_CONNECTION_STRING>";
+        
+        // Your ACS resource phone number will act as source number to start outbound call
+        var acsPhonenumber = "<ACS_PHONE_NUMBER>";
+
+        // Target phone number you want to receive the call.
+        var targetPhonenumber = "<TARGET_PHONE_NUMBER>";
+
+        // Base url of the app
+        var callbackUriHost = "<CALLBACK_URI_HOST_WITH_PROTOCOL>";
+        
+        var callAutomationClient = new CallAutomationClient(acsConnectionString);
+        var app = builder.Build();
+
+        app.MapGet("/outboundCall", async (ILogger<Program> logger) =>
+        {
+            var target = new PhoneNumberIdentifier(targetPhonenumber);
+            var caller = new PhoneNumberIdentifier(acsPhonenumber);
+            var callInvite = new CallInvite(target, caller);
+            var createCallResult = await callAutomationClient.CreateCallAsync(callInvite, new Uri(callbackUriHost + "/api/callbacks"));
+            logger.LogInformation("CreateCallAsync result: {createCallResult}", createCallResult);
+        });
+
+        app.MapPost("/api/callbacks", async (CloudEvent[] cloudEvents, ILogger<Program> logger) =>
+        {
+            foreach (var cloudEvent in cloudEvents)
+            {
+                CallAutomationEventBase parsedEvent = CallAutomationEventParser.Parse(cloudEvent);
+                logger.LogInformation("Received event {eventName} for call connection id {callConnectionId}", parsedEvent?.GetType().Name, parsedEvent?.CallConnectionId);
+                var callConnection = callAutomationClient.GetCallConnection(parsedEvent?.CallConnectionId);
+                var callMedia = callConnection.GetCallMedia();
+
+                if (parsedEvent is CallConnected)
+                {
+                    // Start continuous DTMF recognition
+                    var targetParticipant = new PhoneNumberIdentifier(targetPhonenumber);
+                    var operationContext = "Customer";
+                    var startContinuousDtmfRecognitionAsyncResult = await callMedia.StartContinuousDtmfRecognitionAsync(targetParticipant, operationContext);
+                    logger.LogInformation("StartContinuousDtmfRecognitionAsync result: {startContinuousDtmfRecognitionAsyncResult}", startContinuousDtmfRecognitionAsyncResult);
+                }
+                else if (parsedEvent is ContinuousDtmfRecognitionToneReceived continuousDtmfRecognitionToneReceived)
+                {
+                    logger.LogInformation("DTMF tone received: {tone}", continuousDtmfRecognitionToneReceived.ToneInfo.Tone.ToChar());
+                    await callConnection.HangUpAsync(true);
+                }
+                else if (parsedEvent is ContinuousDtmfRecognitionToneFailed continuousDtmfRecognitionToneFailed)
+                {
+                    logger.LogInformation("StartContinuousDtmfRecognitionAsync failed with ResultInformation: {resultInformation}", continuousDtmfRecognitionToneFailed.ResultInformation);
+                    await callConnection.HangUpAsync(true);
+                }
+            }
+            return Results.Ok();
+        }).Produces(StatusCodes.Status200OK);
+
+        app.UseStaticFiles();
+
+        app.Run();
+    }
+}
---
+++ CallAutomation_ContinuousDtmfRecognition/readme.md
@@ -0,0 +1,47 @@
+﻿|page_type|languages|products
+|---|---|---|
+|sample|<table><tr><td>csharp</tr></td></table>|<table><tr><td>azure</td><td>azure-communication-services</td></tr></table>|
+
+# Call Automation - Continuous Dtmf Recognition Sample
+
+This sample application shows how the Azure Communication Services  - Call Automation SDK can be used to build IVR related solutions. 
+It makes an outbound call to a phone number and collects DTMF tones. The application is a web-based application built on .Net7 framework.
+
+## Prerequisites
+
+- Create an Azure account with an active subscription. For details, see [Create an account for free](https://azure.microsoft.com/free/)
+- Create an Azure Communication Services resource. For details, see [Create an Azure Communication Resource](https://docs.microsoft.com/azure/communication-services/quickstarts/create-communication-resource). You will need to record your resource **connection string** for this sample.
+- Get a phone number for your new Azure Communication Services resource. For details, see [Get a phone number](https://learn.microsoft.com/en-us/azure/communication-services/quickstarts/telephony/get-phone-number?tabs=windows&pivots=programming-language-csharp)
+- Create and host a Azure Dev Tunnel. Instructions [here](https://learn.microsoft.com/en-us/azure/developer/dev-tunnels/get-started)
+- [.NET7 Framework](https://dotnet.microsoft.com/en-us/download/dotnet/7.0) (Make sure to install version that corresponds with your visual studio instance, 32 vs 64 bit)
+
+## Before running the sample for the first time
+
+1. Open an instance of PowerShell, Windows Terminal, Command Prompt or equivalent and navigate to the directory that you would like to clone the sample to.
+2. git clone `https://github.com/Azure-Samples/Communication-Services-dotnet-quickstarts.git`.
+3. Navigate to `CallAutomation_SendDtmfTones` folder and open `CallAutomation_SendDtmfTones.sln` file.
+
+### Setup and host your Azure DevTunnel
+
+[Azure DevTunnels](https://learn.microsoft.com/en-us/azure/developer/dev-tunnels/overview) is an Azure service that enables you to share local web services hosted on the internet. Use the commands below to connect your local development environment to the public internet. This creates a tunnel with a persistent endpoint URL and which allows anonymous access. We will then use this endpoint to notify your application of calling events from the ACS Call Automation service.
+
+```bash
+devtunnel create --allow-anonymous
+devtunnel port create -p 8080
+devtunnel host
+```
+### Configuring application
+
+Open the Program.cs file to configure the following settings
+
+1. `acsConnectionString`: Azure Communication Service resource's connection string.
+2. `acsPhonenumber`: Phone number associated with the Azure Communication Service resource. For e.g. "+1425XXXAAAA"
+3. `targetPhonenumber`: Target phone number to add in the call. For e.g. "+1425XXXAAAA".
+4. `callbackUriHost`: Base url of the app. (For local development replace the dev tunnel url).
+
+### Run app locally
+
+1. Run the `CallAutomation_SendDtmfTones` project.
+2. Open `http://localhost:8080/index.html` in a Web browser.
+3. To initiate the call, click on the `Place a call!` button.
+4. On the target phone, enter DTMF tones and watch the application console.
