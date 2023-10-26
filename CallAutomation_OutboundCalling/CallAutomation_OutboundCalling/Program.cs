d53fb6c912f8b85557185634992344d0b47f5cd1
Vinothini Dharmaraj <v-vdharmaraj@microsoft.com>
2023-10-25 23:34:55 -0700

    added the tts and stt feature to outbound calling

--- CallAutomation_OutboundCalling/CallAutomation_OutboundCalling/Program.cs
+++ CallAutomation_OutboundCalling/CallAutomation_OutboundCalling/Program.cs
@@ -3,8 +3,6 @@ using Azure.Communication.CallAutomation;
 using Azure.Messaging;
 using Azure.Messaging.EventGrid;
 using Azure.Messaging.EventGrid.SystemEvents;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
@@ -22,10 +20,33 @@ var targetPhonenumber = "<TARGET_PHONE_NUMBER>";
 // Base url of the app
 var callbackUriHost = "<CALLBACK_URI_HOST_WITH_PROTOCOL>";
 
+// Your cognitive service endpoint
+var cognitiveServiceEndpoint = "<COGNITIVE_SERVICE_ENDPOINT>";
+
 // This will be set by fileStatus endpoints
 string recordingLocation = "";
 
 string recordingId = "";
+
+// text to play
+const string SpeechToTextVoice = "en-US-NancyNeural";
+const string MainMenu =
+    """ 
+    Hello this is Contoso Bank, we’re calling in regard to your appointment tomorrow 
+    at 9am to open a new account. Please confirm if this time is still suitable for you or if you would like to cancel. 
+    This call is recorded for quality purposes.
+    """;
+const string ConfirmedText = "Thank you for confirming your appointment tomorrow at 9am, we look forward to meeting with you.";
+const string CancelText = """
+Your appointment tomorrow at 9am has been cancelled. Please call the bank directly 
+if you would like to rebook for another date and time.
+""";
+const string CustomerQueryTimeout = "I’m sorry I didn’t receive a response, please try again.";
+const string NoResponse = "I didn't receive an input, we will go ahead and confirm your appointment. Goodbye";
+const string InvalidAudio = "I’m sorry, I didn’t understand your response, please try again.";
+const string ConfirmChoiceLabel = "Confirm";
+const string CancelChoiceLabel = "Cancel";
+const string RetryContext = "retry";
 
 CallAutomationClient callAutomationClient = new CallAutomationClient(acsConnectionString);
 var app = builder.Build();
@@ -34,8 +55,14 @@ app.MapPost("/outboundCall", async (ILogger<Program> logger) =>
 {
     PhoneNumberIdentifier target = new PhoneNumberIdentifier(targetPhonenumber);
     PhoneNumberIdentifier caller = new PhoneNumberIdentifier(acsPhonenumber);
+    var callbackUri = new Uri(callbackUriHost + "/api/callbacks");
     CallInvite callInvite = new CallInvite(target, caller);
-    CreateCallResult createCallResult = await callAutomationClient.CreateCallAsync(callInvite, new Uri(callbackUriHost + "/api/callbacks"));
+    var createCallOptions = new CreateCallOptions(callInvite, callbackUri)
+    {
+        CognitiveServicesEndpoint = new Uri(cognitiveServiceEndpoint)
+    };
+
+    CreateCallResult createCallResult = await callAutomationClient.CreateCallAsync(createCallOptions);
 
     logger.LogInformation($"Created call with connection id: {createCallResult.CallConnectionProperties.CallConnectionId}");
 });
@@ -45,55 +72,68 @@ app.MapPost("/api/callbacks", async (CloudEvent[] cloudEvents, ILogger<Program> 
     foreach (var cloudEvent in cloudEvents)
     {
         CallAutomationEventBase parsedEvent = CallAutomationEventParser.Parse(cloudEvent);
-        logger.LogInformation($"{parsedEvent?.GetType().Name} parsedEvent received for call connection id: {parsedEvent?.CallConnectionId}");
+        logger.LogInformation(
+                    "Received call event: {type}, callConnectionID: {connId}, serverCallId: {serverId}",
+                    parsedEvent.GetType(),
+                    parsedEvent.CallConnectionId,
+                    parsedEvent.ServerCallId);
+
         var callConnection = callAutomationClient.GetCallConnection(parsedEvent.CallConnectionId);
         var callMedia = callConnection.GetCallMedia();
 
-        if (parsedEvent is CallConnected)
+        if (parsedEvent is CallConnected callConnected)
         {
             logger.LogInformation($"Start Recording...");
             CallLocator callLocator = new ServerCallLocator(parsedEvent.ServerCallId);
             var recordingResult = await callAutomationClient.GetCallRecording().StartAsync(new StartRecordingOptions(callLocator));
             recordingId = recordingResult.Value.RecordingId;
 
+            var choices = GetChoices();
+
             // prepare recognize tones
-            CallMediaRecognizeDtmfOptions callMediaRecognizeDtmfOptions = new CallMediaRecognizeDtmfOptions(new PhoneNumberIdentifier(targetPhonenumber), maxTonesToCollect: 1);
-            callMediaRecognizeDtmfOptions.Prompt = new FileSource(new Uri(callbackUriHost + "/audio/MainMenu.wav"));
-            callMediaRecognizeDtmfOptions.InterruptPrompt = true;
-            callMediaRecognizeDtmfOptions.InitialSilenceTimeout = TimeSpan.FromSeconds(5);
+            var recognizeOptions = GetMediaRecognizeChoiceOptions(MainMenu, targetPhonenumber, choices);
 
             // Send request to recognize tones
-            await callMedia.StartRecognizingAsync(callMediaRecognizeDtmfOptions);
+            await callMedia.StartRecognizingAsync(recognizeOptions);
         }
         if (parsedEvent is RecognizeCompleted recognizeCompleted)
         {
-            // Play audio once recognition is completed sucessfully
-            string selectedTone = ((DtmfResult)recognizeCompleted.RecognizeResult).ConvertToString();
+            var choiceResult = recognizeCompleted.RecognizeResult as ChoiceResult;
+            var labelDetected = choiceResult?.Label;
+            var phraseDetected = choiceResult?.RecognizedPhrase;
+            // If choice is detected by phrase, choiceResult.RecognizedPhrase will have the phrase detected, 
+            // If choice is detected using dtmf tone, phrase will be null 
+            logger.LogInformation("Recognize completed succesfully, labelDetected={labelDetected}, phraseDetected={phraseDetected}", labelDetected, phraseDetected);
+            var textToPlay = labelDetected.Equals(ConfirmChoiceLabel, StringComparison.OrdinalIgnoreCase) ? ConfirmedText : CancelText;
 
-            switch (selectedTone)
-            {
-                case "1":
-                    await callMedia.PlayToAllAsync(new FileSource(new Uri(callbackUriHost + "/audio/Confirmed.wav")));
-                    break;
-
-                case "2":
-                    await callMedia.PlayToAllAsync(new FileSource(new Uri(callbackUriHost + "/audio/Goodbye.wav")));         
-                    break;
-
-                default:
-                    //invalid tone
-                    await callMedia.PlayToAllAsync(new FileSource(new Uri(callbackUriHost + "/audio/Invalid.wav")));
-                    break;
-            }
+            await HandlePlayAsync(callMedia, textToPlay);
         }
         if (parsedEvent is RecognizeFailed recognizeFailedEvent)
         {
-            logger.LogInformation($"RecognizeFailed parsedEvent received for call connection id: {parsedEvent.CallConnectionId}");
-
-            // Check for time out, and then play audio message
-            if (recognizeFailedEvent.ReasonCode.Equals(MediaEventReasonCode.RecognizeInitialSilenceTimedOut))
+            var context = recognizeFailedEvent.OperationContext;
+            if (!string.IsNullOrWhiteSpace(context) && context.Equals(RetryContext, StringComparison.OrdinalIgnoreCase))
             {
-                await callMedia.PlayToAllAsync(new FileSource(new Uri(callbackUriHost + "/audio/Timeout.wav")));
+                await HandlePlayAsync(callMedia, NoResponse);
+            }
+            else
+            {
+                var resultInformation = recognizeFailedEvent.ResultInformation;
+                logger.LogError("Encountered error during recognize, message={msg}, code={code}, subCode={subCode}",
+                    resultInformation?.Message,
+                    resultInformation?.Code,
+                    resultInformation?.SubCode);
+
+                var reasonCode = recognizeFailedEvent.ReasonCode;
+                string replyText = reasonCode switch
+                {
+                    var _ when reasonCode.Equals(MediaEventReasonCode.RecognizePlayPromptFailed) ||
+                    reasonCode.Equals(MediaEventReasonCode.RecognizeInitialSilenceTimedOut) => CustomerQueryTimeout,
+                    var _ when reasonCode.Equals(MediaEventReasonCode.RecognizeIncorrectToneDetected) => InvalidAudio,
+                    _ => CustomerQueryTimeout,
+                };
+
+                var recognizeOptions = GetMediaRecognizeChoiceOptions(replyText, targetPhonenumber, GetChoices(), RetryContext);
+                await callMedia.StartRecognizingAsync(recognizeOptions);
             }
         }
         if ((parsedEvent is PlayCompleted) || (parsedEvent is PlayFailed))
@@ -142,10 +182,52 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseStaticFiles(new StaticFileOptions
+CallMediaRecognizeChoiceOptions GetMediaRecognizeChoiceOptions(string content, string targetParticipant, List<RecognitionChoice> choices, string context = "")
 {
-    FileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "audio")),
-    RequestPath = "/audio"
-});
+    var playSource = new TextSource(content) { VoiceName = SpeechToTextVoice };
+
+    var recognizeOptions =
+        new CallMediaRecognizeChoiceOptions(targetParticipant: new PhoneNumberIdentifier(targetParticipant), choices)
+        {
+            InterruptCallMediaOperation = false,
+            InterruptPrompt = false,
+            InitialSilenceTimeout = TimeSpan.FromSeconds(10),
+            Prompt = playSource,
+            OperationContext = context
+        };
+
+    return recognizeOptions;
+}
+
+List<RecognitionChoice> GetChoices()
+{
+    return new List<RecognitionChoice> {
+            new RecognitionChoice("Confirm", new List<string> {
+                "Confirm",
+                "First",
+                "One"
+            }) {
+                Tone = DtmfTone.One
+            },
+            new RecognitionChoice("Cancel", new List<string> {
+                "Cancel",
+                "Second",
+                "Two"
+            }) {
+                Tone = DtmfTone.Two
+            }
+        };
+}
+
+async Task HandlePlayAsync(CallMedia callConnectionMedia, string text)
+{
+    // Play goodbye message
+    var GoodbyePlaySource = new TextSource(text)
+    {
+        VoiceName = "en-US-NancyNeural"
+    };
+
+    await callConnectionMedia.PlayToAllAsync(GoodbyePlaySource);
+}
 
 app.Run();
