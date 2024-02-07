a7b3ae764736b152d9f204327ad8a5c94ff965e0
Yassir Bisteni <yassirb@microsoft.com>
2024-02-07 11:20:47 -0600

    Minor fix

--- RawVideo/UIHelper.cs
+++ RawVideo/UIHelper.cs
@@ -8,28 +8,7 @@ namespace RawVideo
     {
         public static Task RunOnUIThread(this Page p, Action a)
         {
-#if WINDOWS_UWP
             return p.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => { a(); }).AsTask();
-#else
-            var tcs = new TaskCompletionSource();
-
-            var queue = p.DispatcherQueue;
-            var result =  queue.TryEnqueue(() =>
-                {
-                    try
-                    {
-                        a();
-                        tcs.SetResult();
-                    }
-                    catch(Exception ex)
-                    {
-                        tcs.SetException(ex);
-                    }
-                });
-                if (!result) tcs.TrySetException(new InvalidOperationException("Failed to queue operation on UI thread"));
-            
-            return tcs.Task;
-#endif
         }
     }
 }
