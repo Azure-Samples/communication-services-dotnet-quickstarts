9bcc1026239c82187a942918bd0119c691348499
Pilli Vamshi <v-pivamshi@microsoft.com>
2025-04-10 12:01:49 +0530

    addd beta6 test sample.

---
+++ CallAutomation_Beta6_Test_sample/ConfigurationRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace CallAutomation_Beta6_Test_sample
+{
+    public class ConfigurationRequest
+    {
+        public string AcsConnectionString { get; set; } = string.Empty;
+        public string AcsPhoneNumber { get; set; } = string.Empty;
+        public string CongnitiveServiceEndpoint { get; set; } = string.Empty;
+        public string CallbackUriHost { get; set; } = string.Empty;
+    }
+}
