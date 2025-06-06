30a49403b0aeac2b1d803bc3a2f5659dd310a49c
Aigerim Beishenbekova (from Dev Box) <aigerimb@microsoft.com>
2025-06-06 15:53:33 +0200

    fixed importing issue

--- EntraIdUsersSupportQuickstart/Program.cs
+++ EntraIdUsersSupportQuickstart/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Azure.Identity;
+﻿using Azure.Identity;
 using Azure.Communication;
 
 namespace EntraIdUsersSupportQuickstart
