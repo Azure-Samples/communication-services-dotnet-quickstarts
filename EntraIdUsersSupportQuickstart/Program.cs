00c46b43fc63ea9588db1c862f0e6e2598e56acf
Aigerim Beishenbekova <aigerimb@microsoft.com>
2025-06-06 15:30:04 +0200

    added entra id users support quickstart

---
+++ EntraIdUsersSupportQuickstart/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Azure.Communication.Common;
+using Azure.Identity;
+
+namespace EntraIdUsersSupportQuickstart
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Azure Communication Services - Obtain Access Token for Entra ID User Quickstart");
+
+            // This code demonstrates how to fetch your Microsoft Entra client ID and tenant ID from environment variables.
+            string clientId = Environment.GetEnvironmentVariable("ENTRA_CLIENT_ID");
+            string tenantId = Environment.GetEnvironmentVariable("ENTRA_TENANT_ID");
+
+            //Initialize InteractiveBrowserCredential for use with AzureCommunicationTokenCredential.
+            var options = new InteractiveBrowserCredentialOptions
+            {
+                TenantId = tenantId,
+                ClientId = clientId,
+            };
+            var entraTokenCredential = new InteractiveBrowserCredential(options);
+
+            // This code demonstrates how to fetch your Azure Communication Services resource endpoint URI
+            // from an environment variable.
+            string resourceEndpoint = Environment.GetEnvironmentVariable("COMMUNICATION_SERVICES_RESOURCE_ENDPOINT");
+
+            // Set up CommunicationTokenCredential to request a Communication Services access token for a Microsoft Entra ID user.
+            var entraTokenCredentialOptions = new EntraCommunicationTokenCredentialOptions(
+                resourceEndpoint: resourceEndpoint,
+                entraTokenCredential: entraTokenCredential)
+            {
+                Scopes = new[] { "https://communication.azure.com/clients/VoIP" }
+            };
+
+            var credential = new CommunicationTokenCredential(entraTokenCredentialOptions);
+            
+            // To obtain a Communication Services access token for Microsoft Entra ID call getToken() function.
+            var accessToken = await credential.GetToken();
+            Console.WriteLine($"Token: {accessToken.Value.Token}");
+        }
+    }
+}
