aa25a0a7ab50ccc7ee8cc24d7f3d44a9fc27f4e7
Aigerim Beishenbekova (from Dev Box) <aigerimb@microsoft.com>
2025-06-20 10:03:54 +0200

    fixed PR comments

--- EntraIdUsersSupportQuickstart/Program.cs
+++ EntraIdUsersSupportQuickstart/Program.cs
@@ -9,29 +9,21 @@ namespace EntraIdUsersSupportQuickstart
         {
             Console.WriteLine("Azure Communication Services - Obtain Access Token for Entra ID User Quickstart");
 
-            // This code demonstrates how to fetch your Microsoft Entra client ID and tenant ID from environment variables.
             string clientId = Environment.GetEnvironmentVariable("ENTRA_CLIENT_ID");
             string tenantId = Environment.GetEnvironmentVariable("ENTRA_TENANT_ID");
+            string resourceEndpoint = Environment.GetEnvironmentVariable("COMMUNICATION_SERVICES_RESOURCE_ENDPOINT");
 
             //Initialize InteractiveBrowserCredential for use with CommunicationTokenCredential.
-            var options = new InteractiveBrowserCredentialOptions
+            var entraTokenCredential = new InteractiveBrowserCredential(new InteractiveBrowserCredentialOptions
             {
                 TenantId = tenantId,
                 ClientId = clientId,
-            };
-            var entraTokenCredential = new InteractiveBrowserCredential(options);
-
-            // This code demonstrates how to fetch your Azure Communication Services resource endpoint URI
-            // from an environment variable.
-            string resourceEndpoint = Environment.GetEnvironmentVariable("COMMUNICATION_SERVICES_RESOURCE_ENDPOINT");
+            });
 
             // Set up CommunicationTokenCredential to request a Communication Services access token for a Microsoft Entra ID user.
             var entraTokenCredentialOptions = new EntraCommunicationTokenCredentialOptions(
                 resourceEndpoint: resourceEndpoint,
-                entraTokenCredential: entraTokenCredential)
-            {
-                Scopes = new[] { "https://communication.azure.com/clients/VoIP" }
-            };
+                entraTokenCredential: entraTokenCredential);
 
             var credential = new CommunicationTokenCredential(entraTokenCredentialOptions);
             
