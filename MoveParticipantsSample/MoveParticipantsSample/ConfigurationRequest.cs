cb91f4f9fd28be7be5d3be61a332fe22e97b26d8
Kishore Uppu (Centific Technologies Inc) <v-kuppu@microsoft.com>
2025-07-11 15:49:24 +0530

    Multiple Dialouts(Move Participants) Sample - From New branch

---
+++ MoveParticipantsSample/MoveParticipantsSample/ConfigurationRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace MoveParticipantsSample
+{
+    public class ConfigurationRequest
+    {
+        public string AcsConnectionString { get; set; } = string.Empty;
+        public string AcsPhoneNumber { get; set; } = string.Empty;
+        // public string CongnitiveServiceEndpoint { get; set; } = string.Empty;
+        public string CallbackUriHost { get; set; } = string.Empty;
+        public string AcsInboundSender { get; set; } = string.Empty;
+        //public string User2 { get; set; } = string.Empty;
+        //public string User3 { get; set; } = string.Empty;
+        //public string ACSUserIdentity2 { get; set; } = string.Empty;
+        //public string ACSUserIdentity3 { get; set; } = string.Empty;
+    }
+}
---
+++ MoveParticipantsSample/MoveParticipantsSample/Helper.cs
@@ -0,0 +1,100 @@
+﻿using Azure.Communication.CallAutomation;
+using System.Net.WebSockets;
+using System.Text;
+using System.Text.Json;
+
+namespace MoveParticipantsSample
+{
+    public static class Helper
+    {
+        public static async Task ProcessRequest(WebSocket webSocket)
+        {
+            try
+            {
+                var buffer = new byte[1024 * 4];
+                var cancellationToken = new CancellationTokenSource(TimeSpan.FromSeconds(60)).Token;
+                WebSocketReceiveResult receiveResult = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), cancellationToken);
+
+                while (!receiveResult.CloseStatus.HasValue)
+                {
+                    string msg = Encoding.UTF8.GetString(buffer, 0, receiveResult.Count);
+
+                    var response = StreamingData.Parse(msg);
+
+                    if (response != null)
+                    {
+                        if (response is AudioMetadata audioMetadata)
+                        {
+                            Console.WriteLine("***************************************************************************************");
+                            Console.WriteLine("MEDIA SUBSCRIPTION ID-->" + audioMetadata.MediaSubscriptionId);
+                            Console.WriteLine("ENCODING-->" + audioMetadata.Encoding);
+                            Console.WriteLine("SAMPLE RATE-->" + audioMetadata.SampleRate);
+                            Console.WriteLine("CHANNELS-->" + audioMetadata.Channels);
+                            //Console.WriteLine("LENGTH-->" + audioMetadata.Length);
+                            Console.WriteLine("***************************************************************************************");
+                        }
+                        if (response is AudioData audioData)
+                        {
+                            Console.WriteLine("***************************************************************************************");
+                            Console.WriteLine("DATA-->" + JsonSerializer.Serialize(audioData.Data));
+                            Console.WriteLine("TIMESTAMP-->" + audioData.Timestamp);
+                            Console.WriteLine("IS SILENT-->" + audioData.IsSilent);
+                            if (audioData.Participant != null && audioData.Participant.RawId != null)
+                            {
+                                Console.WriteLine("Participant Id-->" + audioData.Participant.RawId);
+                            }
+
+                            Console.WriteLine("***************************************************************************************");
+                        }
+
+                        if (response is TranscriptionMetadata transcriptionMetadata)
+                        {
+                            Console.WriteLine("***************************************************************************************");
+                            Console.WriteLine("TRANSCRIPTION SUBSCRIPTION ID-->" + transcriptionMetadata.TranscriptionSubscriptionId);
+                            Console.WriteLine("LOCALE-->" + transcriptionMetadata.Locale);
+                            Console.WriteLine("CALL CONNECTION ID--?" + transcriptionMetadata.CallConnectionId);
+                            Console.WriteLine("CORRELATION ID-->" + transcriptionMetadata.CorrelationId);
+                            Console.WriteLine("***************************************************************************************");
+                        }
+                        if (response is TranscriptionData transcriptionData)
+                        {
+                            Console.WriteLine("***************************************************************************************");
+                            Console.WriteLine("TEXT-->" + transcriptionData.Text);
+                            Console.WriteLine("FORMAT-->" + transcriptionData.Format);
+                            Console.WriteLine("OFFSET-->" + transcriptionData.Offset);
+                            Console.WriteLine("DURATION-->" + transcriptionData.Duration);
+                            Console.WriteLine("PARTICIPANT-->" + transcriptionData.Participant.RawId);
+                            Console.WriteLine("CONFIDENCE-->" + transcriptionData.Confidence);
+                            Console.WriteLine("RESULT STATUS-->" + transcriptionData.ResultState);
+                            foreach (var word in transcriptionData.Words)
+                            {
+                                Console.WriteLine("WORDS TEXT-->" + word.Text);
+                                Console.WriteLine("WORDS OFFSET-->" + word.Offset);
+                                Console.WriteLine("WORDS DURATION-->" + word.Duration);
+                            }
+                            Console.WriteLine("***************************************************************************************");
+                        }
+                    }
+
+                    await webSocket.SendAsync(
+                        new ArraySegment<byte>(buffer, 0, receiveResult.Count),
+                        receiveResult.MessageType,
+                        receiveResult.EndOfMessage,
+                        CancellationToken.None);
+
+                    receiveResult = await webSocket.ReceiveAsync(
+                     new ArraySegment<byte>(buffer), CancellationToken.None);
+                }
+
+                await webSocket.CloseAsync(receiveResult.CloseStatus.Value, receiveResult.CloseStatusDescription, CancellationToken.None);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception -> {ex}");
+            }
+            finally
+            {
+            }
+        }
+    }
+}
